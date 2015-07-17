c6d509a46364e576c0d99bb4f3706d2a5d2ff1a4
Vikram Dighe <vikram@swiftsignal.com>
2015-07-17 18:02:42 -0400

    RuboCop fixes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,45 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716060920) do
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+ActiveRecord::Schema.define(version: 20_150_716_060_920) do
+  create_table 'locations', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "routes", force: :cascade do |t|
-    t.integer  "section_id"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "rating"
+  create_table 'routes', force: :cascade do |t|
+    t.integer 'section_id'
+    t.string 'name'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.integer 'rating'
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.integer  "location_id"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'sections', force: :cascade do |t|
+    t.integer 'location_id'
+    t.string 'name'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'email',                  default: '', null: false
+    t.string 'encrypted_password',     default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count',          default: 0,  null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.datetime 'created_at',                          null: false
+    t.datetime 'updated_at',                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
+  add_index 'users', ['email'], name: 'index_users_on_email', unique: true
+  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
 end
